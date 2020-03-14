e4aa80803f9d048dd2dcfa56c6bac48a017b2be2
naqvitalha <talha.naqvi@flipkart.com>
2020-03-14 12:02:03 +0530

    added more tests around min/max

--- src/VisibleWindowHelper.test.ts
+++ src/VisibleWindowHelper.test.ts
@@ -15,7 +15,7 @@ function getHeightTracker(size: number, dim: number): Array<number[]> {
     return (new Array(size)).fill([dim, dim]);
 }
 
-test("Window Full: Basic min max, starting at 0 offset", ()=>{
+test("Min/Max: Window Full, starting at 0 offset", ()=>{
     const itemCount = 20;
     let VisibleWindow = getVisibleWindow({ size: 500 });
     const heights = getHeightTracker(itemCount, 50);
@@ -25,21 +25,108 @@ test("Window Full: Basic min max, starting at 0 offset", ()=>{
     //While intersecting
     VisibleWindow = getVisibleWindow({ size: 520 });
     updateVisibleWindow(0, itemCount, 1, heights, VisibleWindow);
-    expect(VisibleWindow).toMatchObject({max: 10});
+    expect(VisibleWindow).toMatchObject({ min: 0, max: 10 });
 
 });
 
-test("Window Full: Basic min max, starting at 0 offset", ()=>{
+test("Min/Max: Window Full (Indexer Change), starting at 0 offset", ()=>{
     const itemCount = 20;
     let VisibleWindow = getVisibleWindow({ size: 500 });
     const heights = getHeightTracker(itemCount, 50);
-    updateVisibleWindow(0, itemCount, 1, heights, VisibleWindow);
+
+    //Exact Match
+    updateVisibleWindow(0, itemCount, 0, heights, VisibleWindow);
     expect(VisibleWindow).toMatchObject({ min: 0, max: 9, size: 500, paddingTillMin: 0, minCaptureOffset: 0});
 
     //While intersecting
     VisibleWindow = getVisibleWindow({ size: 520 });
+    updateVisibleWindow(0, itemCount, 0, heights, VisibleWindow);
+    expect(VisibleWindow).toMatchObject({ min:0, max: 10 });
+
+});
+
+test("Min/Max: Window Half Full", ()=>{
+    const itemCount = 3;
+    let VisibleWindow = getVisibleWindow({ size: 500 });
+    const heights = getHeightTracker(itemCount, 50);
     updateVisibleWindow(0, itemCount, 1, heights, VisibleWindow);
-    expect(VisibleWindow).toMatchObject({max: 10});
+    expect(VisibleWindow).toMatchObject({ min:0, max: 2 });
+
+    VisibleWindow = getVisibleWindow({ size: 500 });
+    updateVisibleWindow(50, itemCount, 1, heights, VisibleWindow);
+    expect(VisibleWindow).toMatchObject({ min: 1, max: 2 });
+});
+
+test("Min/Max: Zero height last element", ()=>{
+    let itemCount = 4;
+    let VisibleWindow = getVisibleWindow({ size: 150 });
+    let heights = getHeightTracker(itemCount, 50);
+    heights[itemCount - 1] = [300, 0];
+    updateVisibleWindow(0, itemCount, 1, heights, VisibleWindow);
+    expect(VisibleWindow).toMatchObject({ min:0, max: 3 });
+});
+
+test("Min/Max: Zero height first element", ()=>{
+    let itemCount = 4;
+    let VisibleWindow = getVisibleWindow({ size: 150 });
+    let heights = getHeightTracker(itemCount, 50);
+    heights[0] = [300, 0];
+    updateVisibleWindow(0, itemCount, 1, heights, VisibleWindow);
+    expect(VisibleWindow).toMatchObject({ min:0, max: 3 });
+});
+
+test("Min/Max/CaptureOffset: Scroll Around", () => {
+    const itemCount = 200;
+    let VisibleWindow = getVisibleWindow({ min: 0, max: 4, size: 500 });
+    const heights = getHeightTracker(itemCount, 100);
+
+    //ScrollDown by 200px
+    updateVisibleWindow(200, itemCount, 1, heights, VisibleWindow);
+    expect(VisibleWindow).toMatchObject({ min: 2, max: 6, minCaptureOffset: 200 });
+
+    //ScrollUp by 50px
+    updateVisibleWindow(150, itemCount, 1, heights, VisibleWindow);
+    expect(VisibleWindow).toMatchObject({ min: 1, max: 6, minCaptureOffset: 100 });
+
+     //ScrollDown by 100px
+     updateVisibleWindow(250, itemCount, 1, heights, VisibleWindow);
+     expect(VisibleWindow).toMatchObject({ min: 2, max: 7, minCaptureOffset: 200 });
+
+      //ScrollDown by 750px
+      updateVisibleWindow(1000, itemCount, 1, heights, VisibleWindow);
+      expect(VisibleWindow).toMatchObject({ min: 10, max: 14, minCaptureOffset: 1000 });
+
+       //ScrollTo 150px
+       updateVisibleWindow(150, itemCount, 1, heights, VisibleWindow);
+       expect(VisibleWindow).toMatchObject({ min: 1, max: 6, minCaptureOffset: 100 });
+
+        //ScrollTo max
+        updateVisibleWindow(19500, itemCount, 1, heights, VisibleWindow);
+        expect(VisibleWindow).toMatchObject({ min: 195, max: 199, minCaptureOffset: 19500 });
+
+        //ScrollToTop
+        updateVisibleWindow(0, itemCount, 1, heights, VisibleWindow);
+        expect(VisibleWindow).toMatchObject({ min: 0, max: 4, minCaptureOffset: 0 });
+
+        //Modify to make first and lastItems of zero height
+        heights[0] = [300, 0];
+        heights[itemCount - 1] = [300, 0];
+
+         //ScrollTo max
+         updateVisibleWindow(19300, itemCount, 1, heights, VisibleWindow);
+         expect(VisibleWindow).toMatchObject({ min: 194, max: 199, minCaptureOffset: 19300 });
+ 
+           //ScrollTo 19499.9px
+           updateVisibleWindow(19299, itemCount, 1, heights, VisibleWindow);
+           expect(VisibleWindow).toMatchObject({ min: 193, max: 199, minCaptureOffset: 19200 });
+
+         //ScrollTo 0.1px
+         updateVisibleWindow(0.5, itemCount, 1, heights, VisibleWindow);
+         expect(VisibleWindow).toMatchObject({ min: 1, max: 6, minCaptureOffset: 0 });
+
+         //ScrollToTop
+         updateVisibleWindow(0, itemCount, 1, heights, VisibleWindow);
+         expect(VisibleWindow).toMatchObject({ min: 0, max: 5, minCaptureOffset: 0 });
 
 });
 
@@ -47,6 +134,8 @@ test("Unwanted mutation to extra params", ()=>{
     const itemCount = 20;
     let VisibleWindow = getVisibleWindow({ size: 500 });
     const heights = getHeightTracker(itemCount, 50);
+    const serializedHeights = JSON.stringify(heights);
     updateVisibleWindow(0, itemCount, 1, heights, VisibleWindow);
     expect(VisibleWindow).toMatchObject({ min: 0, max: 9, size: 500, paddingTillMin: 0, minCaptureOffset: 0});
+    expect(JSON.stringify(heights)).toBe(serializedHeights);
 });
--- src/VisibleWindowHelper.ts
+++ src/VisibleWindowHelper.ts
@@ -6,7 +6,7 @@ export function updateVisibleWindow(offset: number, itemCount: number, indexer: 
         for(let i = visibleWindow.min; i < itemCount; i++) {
             deltaMin -= heightTracker[i][indexer];
             if(deltaMin <= 0) {
-                visibleWindow.min = i;
+                visibleWindow.min = deltaMin < 0 ? i : Math.min (itemCount - 1, i + 1);
                 visibleWindow.minCaptureOffset = offset + deltaMin;
                 break;
             }
@@ -17,6 +17,7 @@ export function updateVisibleWindow(offset: number, itemCount: number, indexer: 
             if(deltaMin >= 0) {
                 visibleWindow.min = i;
                 visibleWindow.minCaptureOffset = offset - deltaMin;
+                break;
             }
         }
     }
@@ -28,5 +29,19 @@ export function updateVisibleWindow(offset: number, itemCount: number, indexer: 
             break;
         }
     }
+    return applyZeroEdgeCorrection(offset, itemCount, indexer, heightTracker, visibleWindow);
+}
+
+function applyZeroEdgeCorrection(offset: number, itemCount: number, indexer: number, heightTracker: Record<number, number[]>, visibleWindow: VisibleWindow): VisibleWindow {
+    if(visibleWindow.min === 1 && offset <= visibleWindow.minCaptureOffset && heightTracker[0][indexer] === 0) {
+        visibleWindow.min = 0;
+    }
+    if(visibleWindow.max === itemCount - 2 && heightTracker[itemCount - 1][indexer] === 0) {
+        visibleWindow.max = itemCount - 1;
+    }
     return visibleWindow;
 }
+
+//#if [TEST]
+export { applyZeroEdgeCorrection };
+//#endif
